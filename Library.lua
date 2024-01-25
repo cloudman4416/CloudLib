64a11888d20aadbd767657c2450d91ccee7bff20
cloudman4416 <134954873+cloudman4416@users.noreply.github.com>
2024-01-25 10:52:18 +0100

    Update Library.lua

--- Library.lua
+++ Library.lua
@@ -1,5 +1,39 @@
+local char = {
+	[1] = "a",
+	[2] = "b",
+	[3] = "c",
+	[4] = "d",
+	[6] = "e",
+	[7] = "f",
+	[8] = "g",
+	[9] = "h"
+	[10] = "i,
+	[11] = "j",
+	[12] = "k",
+	[13] = "l",
+	[14] = "m",
+	[15] = "n",
+	[16] = "o",
+	[17] = "p",
+	[18] = "q,
+	[19] = "r",
+	[20] = "s",
+	[21] = "t"
+}
+function randomstring(len)
+	local string = ""
+	for i = 1, len do
+		string = string .. char[math.ceil(math.random().21)]
+	end
+	return string
+end
 local CloudLib = {}
-function CloudLib:Nigger()
-  print("Nigger")
+function CloudLib:MakeWindow(settings)
+  local gui = Instance.new("ScreenGui")
+  local Window = Instance.new("Frame")
+	Window.Parent = gui
+	-- ui round border
+  gui.Name = randomstring(10)
+	gui.Parent = gethui() or game.CoreGui
 end
 return CloudLib
